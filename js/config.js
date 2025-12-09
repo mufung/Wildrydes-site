0d93b71d40767813d2e575eb9619247d940a761b
Mufung Angelbell Mbuyeh <mufungangelbell@gmail.com>
2025-12-09 13:53:00 +0100

    Update userPoolClientId in config.js

--- js/config.js
+++ js/config.js
@@ -1,12 +1,13 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_kWOilOYXf', // e.g. us-east-1_uXboG5pAb
-        userPoolClientId: '5jctpdt5ad1lvg56o9aopovs4e', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '58fl1er6di12e79rdt2c5kchov', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-1
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
