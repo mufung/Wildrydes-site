67786ca2674fb65dad381b2a9f6160a166dd84f3
Mufung Angelbell Mbuyeh <mufungangelbell@gmail.com>
2025-12-09 13:10:00 +0100

    Update AWS Cognito configuration values

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_AbK770b4C', // e.g. us-east-1_uXboG5pAb
+        userPoolClientId: '2shddfqf5p1t90umkt95m5ud1v', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-1
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
