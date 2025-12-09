0ece73dbcea62541b2a6a2276bf2d269f2073cda
Mufung Angelbell Mbuyeh <mufungangelbell@gmail.com>
2025-12-09 14:08:28 +0100

    Clear sensitive data from config.js

--- js/config.js
+++ js/config.js
@@ -1,13 +1,14 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_kWOilOYXf', // e.g. us-east-1_uXboG5pAb
-        userPoolClientId: '58fl1er6di12e79rdt2c5kchov', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-1' // e.g. us-east-1
+        userPoolId: '', // e.g. us-east-1_uXboG5pAb
+        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: '' // e.g. us-east-1
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
