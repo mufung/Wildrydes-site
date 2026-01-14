9481f7e38ad8470ffe78fead6519fa1647bcfca7
Mufung Angelbell Mbuyeh <mufungangelbell@gmail.com>
2026-01-14 17:04:28 +0100

    Correct config object formatting in config.js
    Fixed formatting issue in the config object.

--- js/config.js
+++ js/config.js
@@ -2,12 +2,13 @@ window._config = {
     cognito: {
         userPoolId: 'us-west-1_ryzzzVwZZ', // e.g. us-east-1_uXboG5pAb
         userPoolClientId: '5762et20hfkh67qcntghvri6dj', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' /us-west-1/ e.g. us-east-1
+        region: ' us-west-1'
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
